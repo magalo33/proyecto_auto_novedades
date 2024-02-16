57dfce7b6de2fd77242df2f4c2f2bbc2f214c11c
Miguel Arcesio Londoño Garzon <61088339+magalo33@users.noreply.github.com>
2024-02-16 08:34:35 -0500

    Update connection.ts

--- serverBackend/bd/connection.ts
+++ serverBackend/bd/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('autonovedades','postgres','Magalo33@88230510',{
+const db = new Sequelize('autonovedades','postgres','xxxxxxxxxxxxxxxxxxxxxxxxxxxx',{
     host: 'localhost',
     port: 5551,
     dialect: 'postgres',
